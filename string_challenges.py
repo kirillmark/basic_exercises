6f540a658b988864e9f99f8f3b84fe4a2ef74b99
Андрей Марк <markandreym@gmail.com>
2024-05-29 20:46:34 +0300

    Выполнил первые 3 задания

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,38 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.lower().count('а'))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+word = word.lower()
+count = 0
+vowels = "аиеёоуыэюя"
+for letter in word:
+    if letter in vowels:
+        count += 1
+print(count)
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(len(sentence.split()))
 
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+for i in sentence.split():
+    print(i[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+count = 0
+for i in sentence.split():
+    count += len(i)
+print(count/len(sentence.split()))
